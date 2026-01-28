7ac7d6c8a996c721e556d5655fe88c4a251f029f
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2026-01-28 13:39:43 +0200

    add table2

--- summary.js
+++ summary.js
@@ -54,6 +54,12 @@ document.addEventListener("DOMContentLoaded", () => {
         periodontitisPermanent: 0,
         periodontitisPermanentChildren: 0,
         periodontitisTemporary: 0,
+        P_vitalTotal: 0, // 18 колонка
+        P_vitalChildren: 0,
+        PtTotal: 0, // 20 колонка
+        PtChildren: 0,
+        depulped: 0,
+        naplast: 0,
         anesthesiaLocal: 0,
         anesthesiaGeneral: 0,
         periodontitis: 0,
@@ -62,6 +68,14 @@ document.addEventListener("DOMContentLoaded", () => {
         PlAm: 0, // 24 колонка
         PlCC: 0, // 25 колонка
         PlLC: 0, // 26 колонка
+        medlikparodont: 0, // 31 колонка
+        kuretazh: 0, // 32 колонка
+        klapteva: 0, // 33 колонка
+        shinuvanya: 0,
+        mucosaTreatment: 0,
+        mucosaTreatmentChildren: 0,
+        column28Sum: 0,
+        column28ChildrenSum: 0,
         uop: 0,
       };
     }
@@ -153,7 +167,60 @@ document.addEventListener("DOMContentLoaded", () => {
       if (filling === "PlCC") groupedByDate[date].PlCC += 1;
       if (filling === "PlLC") groupedByDate[date].PlLC += 1;
     });
+    const procedureColumns = ["10-1", "10-2", "10-3"];
+    groupedByDate[date].column28ChildrenSum = 0;
+    procedureColumns.forEach((col) => {
+      const procedure = row[col];
 
+      const isChild = age <= 17;
+      if (!procedure) return;
+
+      // P_вітально_хірургічно
+      if (procedure === "P_вітально_хірургічно") {
+        groupedByDate[date].P_vitalTotal += 1;
+        if (age <= 17) groupedByDate[date].P_vitalChildren += 1;
+      }
+
+      // Pt
+      if (procedure === "Pt") {
+        groupedByDate[date].PtTotal += 1;
+        if (age <= 17) groupedByDate[date].PtChildren += 1;
+      }
+      if (procedure === "депульповано_зубів") {
+        groupedByDate[date].depulped += 1;
+      }
+      if (procedure === "зняття_напластувань") {
+        groupedByDate[date].naplast += 1;
+      }
+      if (procedure === "медикаментозне_лікування_пародонту") {
+        groupedByDate[date].medlikparodont += 1;
+      }
+      if (procedure === "кюретаж") {
+        groupedByDate[date].kuretazh += 1;
+      }
+      if (procedure === "клаптева_операція") {
+        groupedByDate[date].klapteva += 1;
+      }
+      if (procedure === "шинування_зубів") {
+        groupedByDate[date].shinuvanya += 1;
+      }
+      if (procedure === "лікування_слизової_рота") {
+        groupedByDate[date].mucosaTreatment += 1;
+        if (age <= 17) groupedByDate[date].mucosaTreatmentChildren += 1;
+      }
+      if (isChild) {
+        if (
+          procedure === "зняття_напластувань" ||
+          procedure === "медикаментозне_лікування_пародонту" ||
+          procedure === "кюретаж" ||
+          procedure === "клаптева_операція" ||
+          procedure === "шинування_зубів" ||
+          procedure === "лікування_слизової_рота"
+        ) {
+          groupedByDate[date].column28ChildrenSum += 1;
+        }
+      }
+    });
     groupedByDate[date].groupSum =
       groupedByDate[date].cariesPermanent +
       groupedByDate[date].cariesTemporary +
@@ -161,6 +228,14 @@ document.addEventListener("DOMContentLoaded", () => {
       groupedByDate[date].pulpitisTemporary +
       groupedByDate[date].periodontitisPermanent +
       groupedByDate[date].periodontitisTemporary;
+
+    groupedByDate[date].column28Sum =
+      groupedByDate[date].naplast +
+      groupedByDate[date].medlikparodont +
+      groupedByDate[date].kuretazh +
+      groupedByDate[date].klapteva +
+      groupedByDate[date].shinuvanya +
+      groupedByDate[date].mucosaTreatment;
   });
 
   const sortedDays = Object.values(groupedByDate).sort((a, b) => {
@@ -184,19 +259,34 @@ document.addEventListener("DOMContentLoaded", () => {
         <td>${day.cariesPermanent}</td>                 <!--9-->
         <td>${day.cariesPermanentChildren}</td>         <!--10-->
         <td>${day.cariesTemporary}</td>                 <!--11-->  
-        <td>${day.pulpitisPermanent}</td>
-        <td>${day.pulpitisPermanentChildren}</td>
-        <td>${day.periodontitisPermanent}</td>
-        <td>${day.periodontitisPermanentChildren}</td>
-        <td>${day.pulpitisTemporary}</td>
-        <td>${day.periodontitisTemporary}</td>
-       ${"<td></td>".repeat(5)}
-       <td>${day.PlC}</td>
-        <td>${day.PlAm}</td>
-        <td>${day.PlCC}</td>
-        <td>${day.PlLC}</td>
-        <td>${day.anesthesiaLocal}/${day.anesthesiaGeneral}</td>
-       ${"<td></td>".repeat(9)}
+        <td>${day.pulpitisPermanent}</td>               <!--12-->
+        <td>${day.pulpitisPermanentChildren}</td>        <!--13--> 
+        <td>${day.periodontitisPermanent}</td>           <!--14-->
+        <td>${day.periodontitisPermanentChildren}</td>  <!--15-->
+        <td>${day.pulpitisTemporary}</td>                <!--16-->
+        <td>${day.periodontitisTemporary}</td>           <!--17-->
+        <td>${day.P_vitalTotal}</td>                     <!--18-->
+        <td>${day.P_vitalChildren}</td>                  <!--19-->
+        <td>${day.PtTotal}</td>                          <!--20-->
+        <td>${day.PtChildren}</td>                      <!--21-->
+        <td>${day.depulped}</td>                           <!--22-->
+        <td>${day.PlC}</td>                              <!--23-->
+        <td>${day.PlAm}</td>                             <!--24-->
+        <td>${day.PlCC}</td>                            <!--25-->
+        <td>${day.PlLC}</td>                               <!--26--> 
+        <td>${day.anesthesiaLocal}/${day.anesthesiaGeneral}</td> <!--27-->
+        <td>${day.column28Sum}</td>                     <!-- 28 колонка -->
+        <td>${day.column28ChildrenSum}</td> <!--29 колонка: сума процедур для дітей до 17 років-->
+        <td>${day.naplast}</td>                              <!--30--> 
+        <td>${day.medlikparodont}</td>                        <!--31-->
+        <td>${day.kuretazh}</td>                             <!--32-->
+        <td>${day.klapteva}</td>                             <!--33-->
+        <td>${day.shinuvanya}</td>                           <!--34-->
+        <td>${day.mucosaTreatment}</td>                      <!--35-->
+        <td>${day.mucosaTreatmentChildren}</td>             <!--36-->
+         
+        
+       
         
       `;
     summaryBody1.appendChild(tr);
